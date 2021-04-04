5191429b76561f1d40cde26f198cecd5fa2b0099
sugurunnba <sugurunnba7@gmail.com>
2021-04-04 21:42:52 +0900

    make:authにてログイン機能追加

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // return view('top');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
